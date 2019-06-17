e04b08463017bb5c7669a46b8aa99216fbc5625c
tongchaoyang <tongchaoyang@yahoo.com>
2019-06-17 09:26:43 -0700

    refactored validation logic

--- src/main/java/home/tong/experiments/Node.java
+++ src/main/java/home/tong/experiments/Node.java
@@ -1,84 +1,133 @@
 package home.tong.experiments;
 
 public class Node {
-	// non-negative id, unique within a tree 
-	private int id;
-	
-	public int value;
-	public Node left;
-	public Node right;
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((left == null) ? 0 : left.hashCode());
-		result = prime * result + ((right == null) ? 0 : right.hashCode());
-		result = prime * result + value;
-		return result;
-	}
+    // non-negative id, unique within a tree
+    private int id;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Node other = (Node) obj;
-		if (left == null) {
-			if (other.left != null)
-				return false;
-		} else if (!left.equals(other.left))
-			return false;
-		if (right == null) {
-			if (other.right != null)
-				return false;
-		} else if (!right.equals(other.right))
-			return false;
-		if (value != other.value)
-			return false;
-		return true;
-	}
+    public int value;
+    public Node left;
+    public Node right;
 
-	public Node() {
-		value = 0;
-		id = 0;
-		left = right = null;
-	}
-	
-	public Node(int id) {
-		value = 0;
-		if (id < 0)
-			throw new IllegalArgumentException("Tree node id must be non-negative integer");
-		
-		this.id = id;
-		left = right = null;
-	}
-	
-	public Node(int id, int v) {
-		if (id < 0)
-			throw new IllegalArgumentException("Tree node id must be non-negative integer");
-		value = v;
-		this.id = id;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((left == null) ? 0 : left.hashCode());
+        result = prime * result + ((right == null) ? 0 : right.hashCode());
+        result = prime * result + value;
+        return result;
+    }
 
-	public int getId() {
-		return id;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Node other = (Node) obj;
+        if (left == null) {
+            if (other.left != null)
+                return false;
+        } else if (!left.equals(other.left))
+            return false;
+        if (right == null) {
+            if (other.right != null)
+                return false;
+        } else if (!right.equals(other.right))
+            return false;
+        if (value != other.value)
+            return false;
+        return true;
+    }
 
-	public void setId(int id) {
-		if (id < 0)
-			throw new IllegalArgumentException("Tree node id must be non-negative integer");
-		this.id = id;
-	}
+    public Node() {
+        value = 0;
+        id = 0;
+        left = right = null;
+    }
 
-	@Override
-	public String toString() {
-		String s = "Node [id=" + id + ", value=" + value + ", left ="; 
-		s += left == null ? "-1, right=" : left.id + ", right=";
-		s += right == null ? "-1]" : right.id + "]";
-		return s;
-	}
+    public Node(int id) {
+        value = 0;
+        if (id < 0)
+            throw new IllegalArgumentException("Tree node id must be non-negative integer");
+
+        this.id = id;
+        left = right = null;
+    }
+
+    public Node(int id, int v) {
+        if (id < 0)
+            throw new IllegalArgumentException("Tree node id must be non-negative integer");
+        value = v;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        if (id < 0)
+            throw new IllegalArgumentException("Tree node id must be non-negative integer");
+        this.id = id;
+    }
+
+    public static void populate(Node n, String s) {
+        s = s.substring(1, s.length()-1);
+        
+        String[] pairs = s.split(",");
+        n.setId(Integer.parseInt(pairs[0].substring(3)));
+        n.value = Integer.parseInt(pairs[1].substring(6));
+        String left = pairs[2].substring(5);
+        if (left.equals("-1")) {
+            n.left = null;
+        }
+        else {
+            n.left = new Node(Integer.parseInt(left));
+        }
+        String right = pairs[3].substring(6);
+        if (right.equals("-1")) {
+            n.right = null;
+        }
+        else {
+            n.right = new Node(Integer.parseInt(right));
+        }
+    }
+
+    // validation: check if each node has id, value, left, right elements
+    // doesn't validate numeric value. that will be done during parsing
+    public static void validate(String s) {
+        boolean valid = true;
+        if (s.charAt(0) == '{' && s.charAt(s.length() - 1) == '}') {
+            String[] pairs = s.split(",");
+            if (!pairs[0].substring(0, 4).equals("{id=")) {
+                valid = false;
+            }
+            if (!pairs[1].substring(0, 6).equals("value=")) {
+                valid = false;
+            }
+            if (!pairs[2].substring(0, 5).equals("left=")) {
+                valid = false;
+            }
+            if (!pairs[3].substring(0, 6).equals("right=")) {
+                valid = false;
+            }
+        } else {
+            valid = false;
+        }
+
+        if (!valid) {
+            throw new IllegalArgumentException(s);
+        }
+    }
+
+    @Override
+    public String toString() {
+        String s = "{id=" + id + ",value=" + value + ",left=";
+        s += left == null ? "-1,right=" : left.id + ",right=";
+        s += right == null ? "-1}" : right.id + "}";
+        return s;
+    }
 }
--- src/test/java/home/tong/experiments/Tests.java
+++ src/test/java/home/tong/experiments/Tests.java
@@ -8,106 +8,106 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Tests {
-	private static Tree t;
-	private static Logger logger = LoggerFactory.getLogger(Tests.class);
-	
-	@BeforeAll
-	static void setup() {
-		/** tree initialization
-		 * 		0
-		 * 	   1  4
-		 *    2 3   5
-		 *         6
-		 */
-		Node root = new Node(0, 100);
-		root.value = 0;
-		root.left = new Node(1, 11);
-		root.left.left = new Node(2, 22);
-		root.left.right = new Node(3, 33);
-		root.right = new Node(4, 44);
-		root.right.right = new Node(5, 55);
-		root.right.right.left = new Node(6, 66);
-		t = new Tree(root);
-	}
-	
-	@AfterAll
-	static void tearDown() {
-		t = null;
-	}
-	
-	@Test
-	void testTreeSerialization() {
-		logger.info("Serialize an empty tree");
-		Tree et = new Tree();
-		String stree = et.serialize();
-		logger.info("Empty tree: " + stree);
-		Tree t1 = Tree.deserialize(stree);
-		Assertions.assertEquals(et, t1, "Deserialized empty tree doesn't match");
-		
-		stree = t.serialize();
-		logger.info("Serilized a 7 node tree: " + stree);
-		Tree t2 = Tree.deserialize(stree);
-		logger.info("Deserialized it back and validate");
-		Assertions.assertEquals(t, t2, "Tree serialization failed");
+    private static Tree t;
+    private static Logger logger = LoggerFactory.getLogger(Tests.class);
+    
+    @BeforeAll
+    static void setup() {
+        /** tree initialization
+         *         0
+         *        1  4
+         *    2 3   5
+         *         6
+         */
+        Node root = new Node(0, 100);
+        root.value = 0;
+        root.left = new Node(1, 11);
+        root.left.left = new Node(2, 22);
+        root.left.right = new Node(3, 33);
+        root.right = new Node(4, 44);
+        root.right.right = new Node(5, 55);
+        root.right.right.left = new Node(6, 66);
+        t = new Tree(root);
+    }
+    
+    @AfterAll
+    static void tearDown() {
+        t = null;
+    }
+    
+    @Test
+    void testTreeSerialization() {
+        logger.info("Serialize an empty tree");
+        Tree et = new Tree();
+        String stree = et.serialize();
+        logger.info("Empty tree: " + stree);
+        Tree t1 = Tree.deserialize(stree);
+        Assertions.assertEquals(et, t1, "Deserialized empty tree doesn't match");
+        
+        stree = t.serialize();
+        logger.info("Serilized a 7 node tree: " + stree);
+        Tree t2 = Tree.deserialize(stree);
+        logger.info("Deserialized it back and validate");
+        Assertions.assertEquals(t, t2, "Tree serialization failed");
 
-		// serialize a one-node tree
-		Node root = new Node(0, 100);
-		Tree t3 = new Tree(root);
-		stree = t3.serialize();
-		logger.info("Serilized a node tree: " + stree);
-		Tree t4 = Tree.deserialize(stree);
-		logger.info("Deserialized it back and validate");
-		Assertions.assertEquals(t3, t4, "Tree serialization failed");
-		
-		/*
-		 * serialize a tree like this: 0
-		 * 	                          1
-		 *                           2
-		 */
-		root.left = new Node(1, 200);
-		root.left.left = new Node(2, 300);
-		stree = t3.serialize();
-		logger.info("Serilized 3 nodes left linear tree: " + stree);
-		t4 = Tree.deserialize(stree);
-		logger.info("Deserialized it back and validate");
-		Assertions.assertEquals(t3, t4, "Tree serialization failed");
+        // serialize a one-node tree
+        Node root = new Node(0, 100);
+        Tree t3 = new Tree(root);
+        stree = t3.serialize();
+        logger.info("Serilized a node tree: " + stree);
+        Tree t4 = Tree.deserialize(stree);
+        logger.info("Deserialized it back and validate");
+        Assertions.assertEquals(t3, t4, "Tree serialization failed");
+        
+        /*
+         * serialize a tree like this: 0
+         *                               1
+         *                           2
+         */
+        root.left = new Node(1, 200);
+        root.left.left = new Node(2, 300);
+        stree = t3.serialize();
+        logger.info("Serilized 3 nodes left linear tree: " + stree);
+        t4 = Tree.deserialize(stree);
+        logger.info("Deserialized it back and validate");
+        Assertions.assertEquals(t3, t4, "Tree serialization failed");
 
-		/*
-		 * serialize a tree like this: 0
-		 * 	                             1
-		 *                                 2
-		 */
-		root.left = null;
-		root.right = new Node(2, 200);
-		root.right.right = new Node(4, 400);
-		stree = t3.serialize();
-		logger.info("Serilized 3 nodes right linear tree: " + stree);
-		t4 = Tree.deserialize(stree);
-		logger.info("Deserialized it back and validate");
-		Assertions.assertEquals(t3, t4, "Tree serialization failed");
+        /*
+         * serialize a tree like this: 0
+         *                                  1
+         *                                 2
+         */
+        root.left = null;
+        root.right = new Node(2, 200);
+        root.right.right = new Node(4, 400);
+        stree = t3.serialize();
+        logger.info("Serilized 3 nodes right linear tree: " + stree);
+        t4 = Tree.deserialize(stree);
+        logger.info("Deserialized it back and validate");
+        Assertions.assertEquals(t3, t4, "Tree serialization failed");
 
-		// serialize t's right sub tree
-		t3 = new Tree(t.root.right);
-		stree = t3.serialize();
-		logger.info("Serilized t's right subtree: " + stree);
-		t4 = Tree.deserialize(stree);
-		logger.info("Deserialized it back and validate");
-		Assertions.assertEquals(t3, t4, "Tree serialization failed");
-	}
-	
-	@Test
-	void testTreeValidation() {
-		final String s = "{this is not a tree}";
-		Assertions.assertThrows(IllegalArgumentException.class, 
-				()->Tree.deserialize(s));
-		String s2 = "{{id=1;value=2;left=-1;right=}}";
-		Assertions.assertThrows(IllegalArgumentException.class, 
-				()->Tree.deserialize(s2));
-		String s3 = "{{id=1;value=2;left=-1;right=-}}";
-		Assertions.assertThrows(IllegalArgumentException.class, 
-				()->Tree.deserialize(s3));
-		String s4 = "{{id=1;value=2a;left=-1;right=-}}";
-		Assertions.assertThrows(IllegalArgumentException.class, 
-				()->Tree.deserialize(s4));
-	}
+        // serialize t's right sub tree
+        t3 = new Tree(t.root.right);
+        stree = t3.serialize();
+        logger.info("Serilized t's right subtree: " + stree);
+        t4 = Tree.deserialize(stree);
+        logger.info("Deserialized it back and validate");
+        Assertions.assertEquals(t3, t4, "Tree serialization failed");
+    }
+    
+    @Test
+    void testTreeValidation() {
+        final String s = "{this is not a tree}";
+        Assertions.assertThrows(IllegalArgumentException.class, 
+                ()->Tree.deserialize(s));
+        String s2 = "{{id=1;value=2;left=-1;right=}}";
+        Assertions.assertThrows(IllegalArgumentException.class, 
+                ()->Tree.deserialize(s2));
+        String s3 = "{{id=1;value=2;left=-1;right=-}}";
+        Assertions.assertThrows(IllegalArgumentException.class, 
+                ()->Tree.deserialize(s3));
+        String s4 = "{{id=1;value=2a;left=-1;right=-}}";
+        Assertions.assertThrows(IllegalArgumentException.class, 
+                ()->Tree.deserialize(s4));
+    }
 }
